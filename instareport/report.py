30218f414e525d76609559658915f654a4bec469
Oleksandr Fedorov <olfe@equinor.com>
2022-03-08 17:42:44 +0200

    Roll accounts

--- instareport/report.py
+++ instareport/report.py
@@ -1,11 +1,11 @@
 """Report target accounts."""
+import os
 from concurrent.futures import ThreadPoolExecutor
 from functools import partial
 from pathlib import Path
 
 from instareport.proxy import ProxyPool
 from instareport.vendor import attack
-
 
 ROOT = Path(__file__).parent.parent
 STUBS = ROOT / "stubs"
@@ -14,15 +14,17 @@ TEST = STUBS / "test.txt"
 def main():
     targets = TEST.read_text().splitlines()
     proxies = ProxyPool()
+    max_workers = min(32, (os.cpu_count() or 1) + 4)
 
-    with ThreadPoolExecutor() as executor:
-        executor.map(partial(report, proxies=proxies), targets)
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        executor.map(partial(report, proxies=proxies, targets=targets), range(max_workers))
 
 
-def report(account, proxies):
-    print("Reporting", account)
+def report(i, targets, proxies):
     while True:
-        attack.report_profile_attack(account, proxies.pop())
+        for account in targets:
+            print("Reporting", account)
+            attack.report_profile_attack(account, proxies.pop())
 
 
 if __name__ == "__main__":
