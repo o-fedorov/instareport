e0b8354c1b15dfb921a2e8650386f50e9765352b
Oleksandr Fedorov <olfe@equinor.com>
2022-03-10 14:03:38 +0200

    Provide a working solution

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
 # instareport
-Report propaganda accounts
+
+Report propaganda accounts on Instagram
+Оскаржуємо пропагандистські аккаунти Інстаграму
+
+## ENG
+
+The script uses Instagram credentials to report the accounts.
+Since it may violate the Instagram terms and conditions, and requires
+multifactor authentication to be disabled,
+it is better to register a dedicated Instagram account and
+use its credentials.
+
+### Usage
+
+Ensure you have [Python](https://www.python.org/downloads/) of at
+least version 3.8, and
+[poetry](https://python-poetry.org/docs/#installation) installed.
+
+Clone the repository.  Open a terminal, navigate to the directory
+of the repo and install the dependencies.
+
+```shell
+poetry install --no-dev
+```
+
+You need to install them only once.
+
+Prepare a text file with accounts, one account per line, like following:
+
+```text
+secured_borders_
+rainbow_nation_us
+_blacktivistt_
+```
+
+Store the accounts in a file like `accounts.txt`.
+
+Then run the command like the one below.  You will be prompted for your
+Instagram username and password.  Note that
+the password is not displayed when you type it in.
+
+```shell
+poetry run instareport channels.txt
+```
+
+## UKR
+
+Скрипт використовує логін та пароль облікового запису Інстаграму,
+щоб скаржитись на аккаунти.  Для використання скрипта рекомендуємо зареєструвати
+окремий обліковий запис, оскільки скрипт потенційно може порушувати
+правила Інстаграму, а також тому що для нормальної роботи скрипта
+багатофакторна аутентифікація має бути відключена (
+сподіваюся, ви підключили багатофакторну аутентифікацію для свого особистого
+аккаунта, і це не коди через СМС).
+
+### Використання
+
+Вам потрібно мати [Python](https://www.python.org/downloads/) версії
+щонайменше 3.8, та [poetry](https://python-poetry.org/docs/#installation).
+
+Клонуйте репозиторій.  Відкрийте термінал / консоль командного рядка,
+перейдіть до директорії репозиторію, і встановіть залежності.
+
+```shell
+poetry install --no-dev
+```
+
+Іх терба встановити лише раз.
+
+Підготуйте текстовий файл із іменами акаунтів, на які бажаєте поскаржитись,
+по одному аккаунту на рядок, як наведено нижче:
+
+```text
+secured_borders_
+rainbow_nation_us
+_blacktivistt_
+```
+
+Збережіть аккаунти у файл, наприклад `accounts.txt`.
+Запустіть команду, як на прикладі нижче.  За запитом, введіть
+ім'я користувача та пароль.  Зауважте, що символи паролю не відображаються
+коли ви їх набираєте.
+
+```shell
+poetry run instareport channels.txt
+```
---
+++ instareport/__main__.py
@@ -0,0 +1,88 @@
+import os
+import time
+from json import JSONDecodeError
+
+import click
+import requests
+
+
+@click.command()
+@click.option("--username", prompt=True, default=lambda: os.environ.get("USER", ""))
+@click.password_option(confirmation_prompt=False)
+@click.option("-w", "--wait", default=3.0, type=float, help="wait time between calls.")
+@click.argument("input_file", type=click.File("r"))
+def report(input_file, username, password, wait=3.0):
+    session = requests.session()
+
+    login_resp = session.post(
+        "https://www.instagram.com/accounts/login/ajax/",
+        headers={
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:97.0) Gecko/20100101 Firefox/97.0",
+            "Accept": "*/*",
+            "Accept-Language": "en-US,en;q=0.8,uk-UA;q=0.5,uk;q=0.3",
+            "Accept-Encoding": "gzip, deflate, br",
+            "X-CSRFToken": "l63IoyUQDFEjGPGkrdZISRYc4UeyB1Mt",
+            "X-Instagram-AJAX": "bc16efce305d-hot",
+            "X-IG-App-ID": "936619743392459",
+            "X-ASBD-ID": "198387",
+            "X-IG-WWW-Claim": "0",
+            "X-Requested-With": "XMLHttpRequest",
+            "Origin": "https://www.instagram.com",
+            "DNT": "1",
+            "Referer": "https://www.instagram.com/",
+            "Cookie": "csrftoken=l63IoyUQDFEjGPGkrdZISRYc4UeyB1Mt; mid=YinRkwAEAAEXlCQkpHtQXyvHlL3g; ig_did=FE79328A-DA5E-41E0-9AC7-7E1D4A506896; ig_nrcb=1",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+        },
+        data={
+            "username": username,
+            "enc_password": f"#PWD_INSTAGRAM_BROWSER:0:{int(time.time())}:{password}",
+            "queryParams": "{}",
+            "optIntoOneTap": "false",
+        },
+    )
+
+    if 'authenticated":true' in login_resp.text or "userId" in login_resp.text:
+        session.headers.update({"X-CSRFToken": login_resp.cookies["csrftoken"]})
+    else:
+        print("\n\nLogin failed:\n", login_resp.text, "\n\n")
+        return
+
+    for target_account in input_file:
+        target_account = target_account.strip()
+
+        print(f"Reporting of {target_account}")
+
+        account_details_resp = session.get(
+            f"https://www.instagram.com/{target_account}/?__a=1"
+        )
+
+        try:
+            account_id = account_details_resp.json()["graphql"]["user"]["id"]
+        except (KeyError, JSONDecodeError):
+            print(
+                "\n\nFailed to get account ID:\n",
+                account_details_resp.status_code,
+                account_details_resp.text,
+                "\n\n",
+            )
+            continue
+
+        report = session.post(
+            f"https://www.instagram.com/users/{account_id}/report/",
+            data={"source_name": "", "reason_id": "5", "frx_context": ""},
+        )
+
+        if '"status":"ok"' in report.text:
+            print("Successfully reported.")
+        else:
+            print("\n\nError:\n", report.text, "\n\n")
+
+    time.sleep(wait)
+
+    print("Done")
+
+
+if __name__ == "__main__":
+    report()
